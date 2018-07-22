acd47b9d431c9b64e5110f2753d3ee949129104f
Joseph Canto <jcanto@byui.edu>
2018-07-22 15:53:35 -0600

    Added background image and some CSS styling

--- SacramentMeetingPlanner/Models/Subjects.cs
+++ SacramentMeetingPlanner/Models/Subjects.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SacramentMeetingPlanner.Models
 {
     public class Subjects
     {
         public int ID { get; set; }
+
+        [Display(Name = "Subject")]
         public string SubjectName { get; set; }
     }
 }
