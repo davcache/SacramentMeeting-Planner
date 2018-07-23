2cc290db76d261549320bb85fc7921c48429a65b
Zac Bell <bellz@byui.edu>
2018-07-22 22:53:36 -0600

    bishopric

--- SacramentMeetingPlanner/Pages/NavViews/BishopricView/Create.cshtml.cs
+++ SacramentMeetingPlanner/Pages/NavViews/BishopricView/Create.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using SacramentMeetingPlanner.Models;
 using SacramentMeetingPlanner.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace SacramentMeetingPlanner.Pages.NavViews.BishopricView
 {
@@ -29,29 +30,19 @@ namespace SacramentMeetingPlanner.Pages.NavViews.BishopricView
         public SelectList RoleNameSL { get; set; }
         public void PopulateRoleDropDownList(PlannerContext _context)
         {
-            string[] namesValue = Enum.GetNames(typeof(Role));
-            string[] namesText = Enum.GetNames(typeof(Role));
-            Role[] values = (Role[])Enum.GetValues(typeof(Role));
-            for (int i = 0; i < namesText.Length; i++)
-            {
-                namesText[i] = namesText[i].ToString().Replace("_", " ");
-            }
-
-            string result1 = string.Join(",", namesValue);
-            string result2 = string.Join(",", namesText);
-            Dictionary<string, string> selectObject = new Dictionary<string, string>();
-            selectObject.Add(result1, result2);
-
-            RoleNameSL = new SelectList(selectObject, "result1", "result2");
-        } 
+            var roleQuery = from d in _context.Role
+                            orderby d.RoleTypeName // Sort by RoleTypeName.
+                            select d.RoleTypeName;
+            RoleNameSL = new SelectList(roleQuery.AsNoTracking(), "RoleID", "RoleTypeName");
+        }
 
         public SelectList MemberNameSL { get; set; }
         public void PopulateRoleMembersDownList(PlannerContext _context)
         {
-            var roleQuery = from d in _context.Member
+            var memberQuery = from d in _context.Member
                             orderby d.MemberName // Sort by name.
                             select d.MemberName;
-            MemberNameSL = new SelectList(roleQuery, "MemberID", "Name");
+            MemberNameSL = new SelectList(memberQuery.AsNoTracking(), "MemberID", "MemberName");
         }
 
         [BindProperty]
