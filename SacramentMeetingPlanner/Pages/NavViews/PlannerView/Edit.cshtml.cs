b354f7c8be395e2e9212faa0e19b393322af5fd1
Joseph Canto <jcanto@byui.edu>
2018-07-23 20:53:43 -0600

    Updated plan edit pages

--- SacramentMeetingPlanner/Pages/NavViews/PlannerView/Edit.cshtml.cs
+++ SacramentMeetingPlanner/Pages/NavViews/PlannerView/Edit.cshtml.cs
@@ -19,24 +19,62 @@ namespace SacramentMeetingPlanner.Pages.NavViews.PlannerView
             _context = context;
         }
 
+        public IActionResult OnGet()
+        {
+            PopulateRoleDropDownList(_context);
+            PopulateSongNamesDownList(_context);
+            PopulateRoleMembersDownList(_context);
+            PopulateSubjectsDownList(_context);
+            return Page();
+        }
+
+        public SelectList RoleNameSL { get; set; }
+        public void PopulateRoleDropDownList(PlannerContext _context, object selectedRole = null)
+        {
+            var roleQuery = from d in _context.Role
+                            orderby d.RoleTypeName // Sort by name.
+                            select d;
+            RoleNameSL = new SelectList(roleQuery, "RoleID", "RoleTypeName", selectedRole);
+        }
+
+        public SelectList SongNameSL { get; set; }
+        public void PopulateSongNamesDownList(PlannerContext _context, object selectedSong = null)
+        {
+            var songQuery = from d in _context.Song
+                            orderby d.SongName // Sort by name.
+                            select d;
+            SongNameSL = new SelectList(songQuery, "SongID", "SongName", selectedSong);
+        }
+
+        public SelectList MemberNameSL { get; set; }
+        public void PopulateRoleMembersDownList(PlannerContext _context, object selectedMember = null)
+        {
+            var memberRoleQuery = from d in _context.Member
+                                  orderby d.MemberName // Sort by name.
+                                  select d;
+            MemberNameSL = new SelectList(memberRoleQuery, "MemberID", "MemberName", selectedMember);
+        }
+
+        public SelectList SubjectNameSL { get; set; }
+        public void PopulateSubjectsDownList(PlannerContext _context, object selectedSubject = null)
+        {
+            var subjectNameQuery = from d in _context.Subject
+                                   orderby d.SubjectName // Sort by name.
+                                   select d;
+            SubjectNameSL = new SelectList(subjectNameQuery, "SubjectID", "SubjectName", selectedSubject);
+        }
+
         [BindProperty]
         public Plans Plans { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        [BindProperty]
+        public SongAssignment SongAssignment { get; set; }
 
-            Plans = await _context.Plans.FirstOrDefaultAsync(m => m.PlansID == id);
+        [BindProperty]
+        public SpeakAssignment SpeakAssignment { get; set; }
 
-            if (Plans == null)
-            {
-                return NotFound();
-            }
-            return Page();
-        }
+        [BindProperty]
+        public Prayer Prayer { get; set; }
 
         public async Task<IActionResult> OnPostAsync()
         {
@@ -46,6 +84,10 @@ namespace SacramentMeetingPlanner.Pages.NavViews.PlannerView
             }
 
             _context.Attach(Plans).State = EntityState.Modified;
+            _context.Attach(SongAssignment).State = EntityState.Modified;
+            _context.Attach(SpeakAssignment).State = EntityState.Modified;
+            _context.Attach(Prayer).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
 
             try
             {
